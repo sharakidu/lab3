e02893ef2afbbadfebb2be9f17347286adcb1bf2
Shabalin Ilya <loopandup@gmail.com>
2025-10-01 13:32:25 +0500

    Now it asks for a lastname

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 name = input("Enter your name: ").strip().capitalize()
+lastname = input("Enter your lastname: ").strip().capitalize()
 if name:
-    print(f"Hello, {name}")
+    print(f"Hello, {name, lastname} ")
 else:
     print("Hello, World")
+if name == "Ilya":
+    print("Have a nice day!")
